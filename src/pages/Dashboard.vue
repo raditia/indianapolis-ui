60107abc9ea7050ce533e15e35d86fe6127eaea8
Raditia Madya <gustiraditia@gmail.com>
2018-03-28 21:40:18 +0700

    adding admin template

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -3,9 +3,7 @@
     <div class="col-sm-12 col-md-12">
       <h3>Welcome !!!</h3>
       <p>
-        Lorem ipsum Ad non ut aliquip Duis minim sit ex esse eiusmod anim minim
-        esse qui mollit elit do pariatur deserunt eu eiusmod tempor tempor sunt
-        fugiat ad dolore reprehenderit do labore ex.
+        MANTAPANJENG
       </p>
     </div>
   </div>
--- src/pages/layouts/Header.vue
+++ src/pages/layouts/Header.vue
@@ -218,7 +218,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="../../assets/images/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs">Bapakmu</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
