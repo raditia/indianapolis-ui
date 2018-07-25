32f2f7f4c2b31172aee0af3cc711d9bad69d689f
Raditia Madya <gustiraditia@gmail.com>
2018-07-25 20:14:38 +0700

    Adding table

--- src/components/TP/CheckStatus.vue
+++ src/components/TP/CheckStatus.vue
@@ -11,11 +11,45 @@
                   <input id="input_search" v-model="cffId" type="text" style="width: 100%"/>
                 </div>
                 <div class="form-group">
-                  <button type="submit" class="btn btn-default" style="float: right">Submit</button>
+                  <button type="submit" class="btn btn-default" style="float: right" 
+                  @click="getCheckStatus; showTable = !showTable">Submit</button>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="col-md-12">
+        <div class="table-responsive">
+          <table class="table table-stripped" v-if="showTable">
+          <thead>
+            <tr>
+              <th>
+                <th scope="col">CFF ID</th>
+                <th scope="col">Merchant Name</th>
+                <th scope="col">Pickup Point</th>
+                <th scope="col">CBM total</th>
+                <th scope="col">Pickup Date</th>
+                <th scope="col">Warehouse</th>
+                <th scope="col">Status</th>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="status in checkStatus" :key="status.cffId">
+              <th scope="row">{{scheduling.cffId}}</th>
+              <td>{{ status.merchantName }}</td>
+              <td>{{ status.pickupPointAddress }}</td>
+              <td>{{ status.cbmTotal }}</td>
+              <td>{{ status.pickupDate }}</td>
+              <td>{{ status.warehouseName }}</td>
+              <td>{{ status.schedulingStatus }}</td>
+            </tr>
+          </tbody>
+        </table>
         </div>
       </div>
     </div>
@@ -24,12 +58,20 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'CheckStatus',
   data () {
     return {
-      cffId: ''
+      cffId: '',
+      showTable: false
     }
+  },
+  computed: {
+    ...mapGetters({
+      checkStatus: 'check_status/checkStatus'
+    })
   },
   methods: {
     getCheckStatus: function () {
